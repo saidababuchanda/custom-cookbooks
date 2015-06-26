a4f289d7aab7a8217e38f5f2435fdc370e858a11
saida babu chanda <saidababu.chanda@gmail.com>
2015-06-26 11:10:29 -0700

    Delete metadata.rb

--- test-env/metadata.rb
+++
@@ -1,6 +0,0 @@
-name             'test-env'
-maintainer       ''
-maintainer_email ''
-description      'tests env variables usage
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
